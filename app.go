abf63efd5a234d6ad18ab24b631b6495794f7fbd
Tim Newsham <tim@fly.io>
2024-11-14 13:34:29 -1000

    add a test app

---
+++ app.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+var t = template.Must(template.New("index.html.tmpl").Parse(`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+<h1>Header</h1>
+<ul>
+  {{range $key, $value := .Header}}
+  <li> {{ $key }}: {{ $value }} </li>
+  {{end}}
+</ul>
+
+<h1>Hello from Fly</h1>
+This is stuff here.
+{{ if .Region }}
+<h2>I'm running in the {{.Region}} region</h2>
+{{end}}
+
+</body>
+</html>
+`))
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := map[string]interface{}{
+			"Region": os.Getenv("FLY_REGION"),
+			"Header": r.Header,
+		}
+
+		t.ExecuteTemplate(w, "index.html.tmpl", data)
+	})
+	log.Println("listening")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/timnewsham/fly-test
+
+go 1.23
